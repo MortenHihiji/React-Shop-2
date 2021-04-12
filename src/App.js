848e2b3d44d6d83e405350d1e0cdea070299f417
unknown <morten.hihiji96@gmail.com>
2021-04-12 21:04:03 +0300

    Fixed unnecessary rerenders in Categories.jsx & added json-server for fake backend

--- src/App.js
+++ src/App.js
@@ -1,54 +1,45 @@
 import React from 'react';
 import axios from 'axios';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router';
 import { setPizzas } from './redux/actions/pizzas';
 
-// function App() {
-//   React.useEffect(() => {
-//     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-//       setPizzas(data.pizzas);
-//     });
-//   }, []);
+function App() {
+  const dispatch = useDispatch();
 
-//   return ;
-// }
-
-class App extends React.Component {
-  componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      this.props.setPizzas(data.pizzas);
+  React.useEffect(() => {
+    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+      console.log(data);
+      dispatch(setPizzas(data));
     });
-  }
+  }, []);
 
-  render() {
-    console.log(this.props);
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Route path="/" render={() => <Home items={this.props.items} />} exact />
-          <Route path="/cart" component={Cart} exact />
-        </div>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" component={Home} exact />
+        <Route path="/cart" component={Cart} exact />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-    filters: state.filters,
-  };
-};
+export default App;
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setPizzas: (items) => dispatch(setPizzas(items)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(
+//   (state) => {
+//     return {
+//       items: state.pizzas.items,
+//       filters: state.filters,
+//     };
+//   },
+//   (dispatch) => {
+//     return {
+//       setPizzas: (items) => dispatch(setPizzas(items)),
+//     };
+//   },
+// )(App);
